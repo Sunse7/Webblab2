5bc8775ffdc85a9e4c47ee69716d598ca1d60187
Sunse7 <sunsetluna@gmail.com>
2020-03-09 16:59:12 +0100

    Added some javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+let key = 'b6hep';
+const xhr = new XMLHttpRequest();
+const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
+const data = JSON.stringify();
+xhr.responseType = 'json';
