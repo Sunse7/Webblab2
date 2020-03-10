aec430383b5d9466779f2c6ea53f5942d5622c65
Sunse7 <sunsetluna@gmail.com>
2020-03-10 13:53:43 +0100

    Moved script tag in HTML, it works now

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let key = 'b6hep';
-const xhr = new XMLHttpRequest();
-const url = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
-const data = JSON.stringify();
-xhr.responseType = 'json';
+const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
+
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function(click) {
+	btn.innerHTML = 'Yay!!';
+});
+
