cf5a919724d7f577dbe413a5726a5caf11393d88
Sunse7 <sunsetluna@gmail.com>
2020-03-11 14:08:17 +0100

    Now javasript works

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let key = 'b6hep';
 const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
 
+const bookNameField = document.getElementById('book-name');
+const bookAuthorField = document.getElementById('book-author-name');
+const addBookButton = document.getElementById('add-book-button');
+addBookButton.addEventListener('click', addBook);
 
-let btn = document.getElementById('btn');
-btn.addEventListener('click', function(click) {
-	btn.innerHTML = 'Yay!!';
-});
+function addBook() {
+	const insertBook = '&op=insert';
+	const bookTitle = '&title=' + bookNameField.value;
+	const bookAuthor = '&author=' + bookAuthorField.value;
+	const endpoint = baseUrl + insertBook + bookTitle + bookAuthor;
+	fetch(endpoint).then(response => response.json).then(json => {console.log(json.status)});
+}
 
