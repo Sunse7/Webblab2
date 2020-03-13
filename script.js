a9c4e1029bdec6465e9a95902fe9e3773fb15318
Sunse7 <sunsetluna@gmail.com>
2020-03-13 11:33:14 +0100

    Add book and view book works

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+let key = 'b6hep';
+const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
+let bookNameField;
+let bookAuthorField;
+let addBookButton;
+let bookList; 
+const tryTimes = 9;
+
 window.addEventListener('load', ()=> {
-	const bookNameField = document.getElementById('book-name');
-	const bookAuthorField = document.getElementById('book-author-name');
-	const addBookButton = document.getElementById('add-book-button');
-	const bookList = document.getElementById('current-book-list');
+	bookNameField = document.getElementById('book-name');
+	bookAuthorField = document.getElementById('book-author-name');
+	addBookButton = document.getElementById('add-book-button');
+	bookList = document.getElementById('current-book-list');
 	addBookButton.addEventListener('click', addBook);
 })
 
-let key = 'b6hep';
-const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php?key=' + key;
-
-function addBook() {
+function addBook(tryTimes) {
+	if (tryTimes <= 0) {
+		console.log('')
+		return;
+	}
 	const insertBook = '&op=insert';
 	const bookTitle = '&title=' + bookNameField.value;
 	const bookAuthor = '&author=' + bookAuthorField.value;
 	const endpoint = baseUrl + insertBook + bookTitle + bookAuthor;
-	fetch(endpoint).then(response => response.json).then(json => {console.log(json.status)});
+	fetch(endpoint).then(response => response.json).then(json => {
+		console.log(json.status)
+		if (json.status === 'success') {
+			console.log('Successfully added book')
+			bookNameField.value = '';
+			bookAuthorField.value = '';
+		}
+		else {
+			return addBook(tryTimes--);
+		}	
+	});
 }
+
 function viewBookList() {
 	const viewBook = '&op=select';
 	const endpoint = baseUrl + viewBook;
@@ -25,7 +45,7 @@ function viewBookList() {
 			json.data.forEach(element => {
 				bookArray.push({title: element.title, author: element.author});
 			});
-						
+			bookList.innerHTML = '';
 			for (let i = 0; i < bookArray.length; i++) {
 				let makeList = document.createElement('li');
 				makeList.innerHTML = `Title: ${bookArray[i].title} Author: ${bookArray[i].author}`;	
